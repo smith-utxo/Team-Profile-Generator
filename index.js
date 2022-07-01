aa856a3a69db8f57d98f314cdecf29735526cb64
Adam <smith.adam937@gmail.com>
2022-07-01 13:04:00 -0500

    initial classes added

---
+++ index.js
@@ -0,0 +1,68 @@
+
+
+class Employee {
+    constructor(name,id,email){
+      this.name = name; 
+      this.id = id; 
+      this.email = email; 
+    }
+        getName(){
+
+        }
+        getId(){
+
+        }
+        getRole(){
+
+        }
+}
+
+  class Engineer {
+    constructor(name, id, email, github){
+      this.name = name; 
+      this.id = id; 
+      this.email = email; 
+      this.github = github; 
+    }
+        getName(){
+
+        }
+        getId(){
+
+        }
+        getRole(){
+
+        }
+        getGithub(){
+
+        }
+        getRole(){
+
+        }
+}
+
+class Intern {
+    constructor(name,id,email, school){
+      this.name = name; 
+      this.id = id; 
+      this.email = email; 
+      this.school = school; 
+    }
+        getName(){
+
+        }
+        getId(){
+
+        }
+        getRole(){
+
+        }
+        getSchool(){
+
+        }
+        getRole(){
+
+        }
+    }
+    // should add validation on inquirer propts to ensure user provided input 
+    
